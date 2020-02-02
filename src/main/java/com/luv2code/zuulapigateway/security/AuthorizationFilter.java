3f6f33510efbf43e69c74830d3c5157f56ccb4da
JackTran791 <congsonit@gmail.com>
2020-02-01 23:16:27 -0600

    Initial commit

---
+++ src/main/java/com/luv2code/zuulapigateway/security/AuthorizationFilter.java
@@ -0,0 +1,70 @@
+package com.luv2code.zuulapigateway.security;
+
+import io.jsonwebtoken.Jwts;
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * @author Jack Tran
+ */
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    private final Environment environment;
+
+    public AuthorizationFilter(AuthenticationManager authManager, Environment environment) {
+        super(authManager);
+        this.environment = environment;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String authorizationHeader = request.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith(Objects.requireNonNull(environment.getProperty("authorization.token.header.prefix")))) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
+        String authorizationHeader = req.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (authorizationHeader == null) {
+            return null;
+        }
+
+        String token = authorizationHeader.replace(Objects.requireNonNull(environment.getProperty("authorization.token.header.prefix")), "");
+
+        System.out.println(environment.getProperty("token.secret"));
+
+        String userId = Jwts.parser()
+                .setSigningKey(environment.getProperty("token.secret"))
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        if (userId == null) {
+            return null;
+        }
+
+        return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+
+    }
+}
